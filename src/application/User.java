a78c0d1363a16383e629766f328c803a42dbc4ed
tony931018 <kning6@asu.edu>
2026-07-09 15:00:02 -0700

    Update User.java

--- src/application/User.java
+++ src/application/User.java
@@ -1,1 +1,50 @@
+package application;
 
+public class User {
+    private String name;
+    private String email;
+    private String role;
+    private String phone;
+    private String address;
+
+    public User(String name, String email, String role) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.phone = "";
+        this.address = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | " + email + " | " + role;
+    }
+}
