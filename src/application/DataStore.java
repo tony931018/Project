2abf6b142435942549086d86db659a9caccb0c25
tony931018 <kning6@asu.edu>
2026-07-10 01:22:56 -0700

    Login is not allowed for everyone because you need to register your account first. Register will actually store your account and password in DataStore. Browse doesn’t have "Report Issue" anymore. “Report Issue” is now in “My Rentals”. Items that can only be reported are those that were rented by you. When you report an issue, you can choose the issue category or write "Other."

--- src/application/DataStore.java
+++ src/application/DataStore.java
@@ -9,7 +9,8 @@ public class DataStore {
     public static ArrayList<Equipment> myListings = new ArrayList<>();
     public static ArrayList<String> reports = new ArrayList<>();
 
-    public static String currentUser = "Current User";
+    public static String currentUser = "";
+    public static String currentUserEmail = "";
 
     public static boolean profileCompleted = false;
     public static String profileName = "";
@@ -27,18 +28,33 @@ public class DataStore {
 
         loaded = true;
 
-        users.add(new User("Admin", "admin@asu.edu", "Admin"));
-        users.add(new User("John Smith", "john@asu.edu", "Renter"));
-        users.add(new User("Samuel", "samuel@asu.edu", "Owner"));
-        users.add(new User("Mark G.", "mark@asu.edu", "Renter"));
-
+        // Only sample equipment is loaded.
+        // Users must register before logging in.
         equipmentList.add(new Equipment("Canon Camera", "Camera & Photo", "Like New", "Samuel", 700));
         equipmentList.add(new Equipment("MacBook Pro", "Computer & Laptop", "Good", "Samuel", 1200));
         equipmentList.add(new Equipment("LG 24 Monitor", "Electronics Devices", "Used", "Samuel", 300));
         equipmentList.add(new Equipment("Gaming Controller", "Gaming Console", "Good", "Samuel", 80));
         equipmentList.add(new Equipment("Samsung Phone", "Electronics Devices", "Used", "Samuel", 500));
+    }
 
-        reports.add("User: Mark G. | Listing: Canon Camera | Issue: Broken item | Status: Pending");
+    public static User findUser(String email, String password) {
+        for (User user : users) {
+            if (user.checkLogin(email, password)) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public static boolean emailExists(String email) {
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static double totalRevenue() {
